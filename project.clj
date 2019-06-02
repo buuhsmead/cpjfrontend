e67571c7d0007611fc7448d1387a3fcdd503b9dc
Huub Daems <huub@redhat.com>
2019-06-03 00:06:02 +0200

    on port 8080 instead 3000

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler cpjfrontend.handler/app}
+  :ring {:handler cpjfrontend.handler/app :port 8080}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
